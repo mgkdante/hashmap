d549819e3f8b0f3a2b218456f0b41b3c28cd1409
yesid <mgkdante@pm.me>
2024-06-08 15:50:23 -0400

    Implemented Linedlists to Hashmap

--- linkedlist.js
+++ linkedlist.js
@@ -53,22 +53,31 @@ class LinkedList {
   }
 
   pop = () => {
+    if (this.count === 0) {
+      return null
+    }
     let current = this.headNode
     if (this.count === 1) {
       this.headNode = null
+      this.tailNode = null
+    } else {
+      for (let i = 0; i < this.count - 2; i++) {
+        current = current.next
+      }
+      current.next = null
+      this.tailNode = current
     }
-    for (let i = 1; i < this.size() - 1; i++) {
-      current = current.next
-    }
-    current.next = null
-    this.tailNode = current
     this.count -= 1
   }
 
   contains = (val) => {
     let current = this.headNode
     while (current) {
-      if (current.val === val) {
+      if (
+        Array.isArray(current.val) &&
+        current.val[0] === val[0] &&
+        current.val[1] === val[1]
+      ) {
         return true
       }
       current = current.next
@@ -78,9 +87,13 @@ class LinkedList {
 
   find = (val) => {
     let current = this.headNode
-    let counter = 1
+    let counter = 0 // Zero-based index
     while (current) {
-      if (current.val === val) {
+      if (
+        Array.isArray(current.val) &&
+        current.val[0] === val[0] &&
+        current.val[1] === val[1]
+      ) {
         return counter
       }
       current = current.next
@@ -98,6 +111,37 @@ class LinkedList {
     }
     string += "null"
     return string
+  }
+
+  removeAt = (index) => {
+    if (index < 0 || index >= this.size()) {
+      return null
+    }
+
+    let current = this.headNode
+    let previous = null
+
+    if (index === 0) {
+      this.headNode = current.next
+      if (this.size() === 1) {
+        this.tailNode = null
+      }
+      this.count -= 1
+      return current.val
+    }
+
+    for (let i = 0; i < index; i++) {
+      previous = current
+      current = current.next
+    }
+
+    previous.next = current.next
+    if (current.next === null) {
+      this.tailNode = previous
+    }
+
+    this.count -= 1
+    return current.val
   }
 }
 
